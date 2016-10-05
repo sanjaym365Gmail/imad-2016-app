a681ec0c2274d0b62a4940520446c3a01153fa9a
sanjaym365Gmail <sanjaym365@gmail.com>
2016-10-05 22:22:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,18 @@
 console.log('Loaded!');
 
+var counter = 0;
 
+var button = document.getElementById("counter");
+
+button.onclick = function() {
+  // Make a request to the counter end point
+  
+  // Capture the response in the variable
+  
+  // Display the value. Render it in the correct span
+  counter = counter + 1;
+  var spanToDisplay = document.getElementById("count");
+  spanToDisplay.innerHTML = counter
+};
 
 
