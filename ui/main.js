f942d65f5a380544cd6183c04b9bbd666a098f3f
sanjaym365Gmail <sanjaym365@gmail.com>
2016-10-05 17:27:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var img = document.getElementById("madi");
 
 function moveRight(){
     marginLeft = marginLeft + 10;
-    img.style,marginLeft = marginLeft +'px';
+    img.style.marginLeft = marginLeft +'px';
 }
 img.onclick = function(){
     var interval = setInterval(moveRight, 100);
