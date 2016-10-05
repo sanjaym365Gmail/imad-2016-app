13f7e30cd6e5de42afc841e6d4a7ac84fba7825d
sanjaym365Gmail <sanjaym365@gmail.com>
2016-10-05 16:52:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element = document.getElementById("main-text");
 
 element.innerHTML = 'New Data';
+
+// Move the madi.png picture
+var img = document.getElementById("madi");
+
+img.onclick = function(){
+    img.style.marginleft = '100px';
+};
