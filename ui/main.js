2ac92bf9153518a0a51152f54083a7359b760b12
sanjaym365Gmail <sanjaym365@gmail.com>
2016-10-06 01:46:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,13 +33,30 @@ var submitBtn = document.getElementById('submit_btn');
 
 submitBtn.onclick = function(){
     //Make a request to the sever and send the names
-    
-    //Capture the list of names and render them
-  var names = ['name1', 'name2', 'name3'];
-  var list = '';
-  for (var i=0; i < names.length; i++){
-      list += '<li>' + names[i] + '</li>';
-  }
-  var nameList = document.getElementById('nameList');
-  nameList.innerHTML = list;
+ // create a request Object
+  var request = new XMLHttpRequest();
+  // Capture the response in the variable
+  request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE)  {
+            //Take some action
+            if (request.status === 200){
+                  //Capture the list of names and render them
+                  var names = request.responseText;
+                  names = JSON.parse(names);
+                  
+                  var list = '';
+                  for (var i=0; i < names.length; i++){
+                      list += '<li>' + names[i] + '</li>';
+                  }
+                  var nameList = document.getElementById('nameList');
+                  nameList.innerHTML = list;       
+                
+            }
+        }
+    }
+     
+  //Make a request
+  request.open('GET', 'http://sanjaym365gmail.imad.hasura-app.io/submit-name?name='+ nameValue,true);
+  request.send(null);
+ 
 };
